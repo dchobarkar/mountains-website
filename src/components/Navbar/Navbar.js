71ae95cb33019854b539ffbe628096b01d370775
Darshan Chobarkar <dchobarkar@gmail.com>
2024-04-11 18:14:36 +0530

    add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+import logo from "../../assets/Logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isMobile, setIsMobile] = useState(false);
+  const [sticky, setSticky] = useState(false);
+
+  // Function to toggle mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  // Handle scroll event
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setSticky(currentScrollY > 200);
+    };
+
+    // Register the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the scroll event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${sticky ? "sticky" : ""}`}>
+      <div className="logo">
+        <img src={logo} alt="Los Angeles Mountains Logo" />
+      </div>
+
+      <ul className={`nav-links ${isMobile && "mobile"}`}>
+        <li>
+          <a href="#history" onClick={() => setIsMobile(false)}>
+            01. HISTORY
+          </a>
+        </li>
+        <li>
+          <a href="#team" onClick={() => setIsMobile(false)}>
+            02. TEAM
+          </a>
+        </li>
+      </ul>
+
+      <button
+        className="mobile-menu-icon"
+        onClick={toggleMobileMenu}
+        style={{ background: "none" }}
+      >
+        {isMobile ? <FaTimes /> : <FaBars />}
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
